ad999a8f008375643e24eb8515502fd2a2b4da1d
Patricia <wambuipatricia3@gmail.com>
2023-03-29 13:21:44 +0300

    Completed Assignments

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
-// Write your code here
+class Breakfast{
+    constructor(food,drink){
+        this.food= food;
+        this.drink=drink;
+    }
+}
+const buffet =new Breakfast("eggs","juice")
+
+
+class Lunch{
+    constructor(salad,soup,drink) {
+        this.salad= salad;
+        this.soup=soup;
+        this.drink=drink;
+
+    }
+}
+const foodie = new Lunch("chicken salad","pumpkin soup","passion juice")
+
+
+class Dinner{
+    #dessert;
+    constructor(salad,soup,entree, dessert) {
+        this.salad=salad;
+        this.soup=soup;
+        this.entree=entree;
+        this.#dessert=dessert;
+    }
+}
+const newDinner = new Dinner("balsamic salad","consomne","filet mignon")
